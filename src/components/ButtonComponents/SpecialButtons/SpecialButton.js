8c269f9cdf330f283b9e611212ae6df6c1c0df71
occludedarp <cmrnmr@gmail.com>
2019-10-09 00:05:51 -0400

    completed MVP

--- src/components/ButtonComponents/SpecialButtons/SpecialButton.js
+++ src/components/ButtonComponents/SpecialButtons/SpecialButton.js
@@ -1,9 +1,14 @@
 import React from "react";
 
-const SpecialButton = () => {
+const SpecialButton = props => {
   return (
-    <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
+    <button
+      className="special_button"
+      onClick={() => props.setSpecialsState(props.specialChar)}
+    >
+      {props.specialChar}
+    </button>
   );
 };
+
+export default SpecialButton;
